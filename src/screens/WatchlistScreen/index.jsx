7188f372583ae7f276016dcb37872dc8152227bc
c3aus3scu <adam.marius@gmail.com>
2022-01-08 04:25:01 +0000

    Bottom Navigation

---
+++ src/screens/WatchlistScreen/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const WatchlistScreen = () => {
+    return (
+        <View>
+            <Text style={{color: 'white'}}>Watchlist</Text>
+        </View>
+    )
+};
+
+export default WatchlistScreen
